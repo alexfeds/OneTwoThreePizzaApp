97cd4fe9d3e2345a63b3f0d1966a6a66d74085cd
alexfeds <alexfeds@gmail.com>
2021-03-14 16:20:46 +0000

    added order components, pizza components, some routing

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 import { CustomerCreateComponent } from './components/customer-create/customer-create.component';
-import { OrderCreateComponent } from './components/order-create/order-create.component';
-import { OrderListComponent } from './components/order-list/order-list.component';
+import { OrderCreateComponent } from './components/order/order-create/order-create.component';
+import { OrderListComponent } from './components/order/order-list/order-list.component';
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
 import { PizzaComponent } from './components/pizza/pizza.component';
+import { OrderComponent } from './components/order/order.component';
 
 
 const routes: Routes = [
-  { path: 'order', component: OrderCreateComponent },
+  { path: 'order', component: OrderComponent },
   { path: 'customers', component: CustomerListComponent },
   { path: 'pizza', component: PizzaComponent }
 ];
