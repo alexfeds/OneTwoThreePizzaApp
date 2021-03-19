91f7faac112c6027b4b38bd6615d199c057f24d1
alexfeds <alexfeds@gmail.com>
2021-03-19 20:11:20 +0000

    init commit layout

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { AppService } from './services/app.service';
 import { MenuItem, MessageService } from 'primeng/api';
 import { CustomersService } from './services/customers-service/customers.service';
 import { Customer } from './services/customers-service/customer';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 
 @Component({
@@ -14,20 +15,39 @@ import { Customer } from './services/customers-service/customer';
 })
 
 
+
 export class AppComponent implements OnInit {
 
   customers: Customer[];
   menuItems: MenuItem[];
+  urlHeading: string;
 
-  constructor(private cusomerService: CustomersService) { }
+
+  constructor(private cusomerService: CustomersService, private router: Router, private activatedRoute: ActivatedRoute) {
+
+
+
+  }
 
   ngOnInit() {
     this.menuItems = [
       { label: 'New Order', icon: 'pi pi-fw pi-pencil', routerLink: ['order'] },
-      { label: 'Customers', icon: 'pi pi-fw pi-users', routerLink: ['customers'] },
       { label: 'Orders', icon: 'pi pi-fw pi-list', routerLink: ['orders'] },
+      { label: 'Customers', icon: 'pi pi-fw pi-users', routerLink: ['customers'] },
       { label: 'New Pizza', icon: 'pi pi-fw pi-file', routerLink: ['pizza'] },
+      { label: 'Pizzas', icon: 'pi pi-fw pi-file', routerLink: ['pizzas'] },
     ];
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        console.log("current url", val.url); // event.url has current url
+
+        this.urlHeading = val.url.substring(1);
+
+
+  
+        // your code will goes here
+      }
+    });
   }
 
   getCustomers() {
